7ee1cca1dbdc0a4538a6b3e52b3050f5061fc406
David Wang <davidwang20000@gmail.com>
2023-04-18 20:10:44 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./scenes/Navbar";
+import { useState } from 'react';
+import './App.css';
+import useMediaQuery from './hooks/useMediaQuery';
+
+function App() {
+
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+    </div>
+  )
+}
+
+export default App
